b408de8f7ec1f57cac16ea4e7ff04f489eafa9ae
goodtimep <879636706@qq.com>
2019-03-07 21:39:33 +0800

    123

--- api/src/app/Common/Tools.php
+++ api/src/app/Common/Tools.php
@@ -22,13 +22,13 @@ class  Tools
     // 数据库查询语句，判断题目中存在的关键字
     //$command = "select Id,Word from keyword where '" . $text . "' like concat('%',Word,'%')";
     $keyarr = $kw->gesKeyWordsByWords($text);
- 
-    for ($i=0;$i<count($keyarr);$i++) {
+
+    for ($i = 0; $i < count($keyarr); $i++) {
       $cnt = substr_count($text, $keyarr[$i]['Word']);
-      $keyarr[$i]["Weight"]=$keyarr[$i]["Weight"]*$cnt;
+      $keyarr[$i]["Weight"] = $keyarr[$i]["Weight"] * $cnt;
     }
-    
-    Tools::SortByKey($keyarr,"Weight",false);
+
+    Tools::SortByKey($keyarr, "Weight", false);
     return $keyarr;
   }
 
@@ -38,41 +38,34 @@ class  Tools
    * @param $key 排序的键值
    * @param $f
    */
-  public static function SortByKey(&$arr,$key,$f)
+  public static function SortByKey(&$arr, $key, $f)
   {
-    $reslut=array();
-    foreach($arr as $item)
-    {
-      for($i=0;$i<count($reslut);$i++)
-      {
-        if(!$f)
-        {
-          if($item[$key]>$reslut[$i][$key]) break;
-          
-        }
-        else {
-          if($item[$key]<$reslut[$i][$key]) break;
-        }
+    $reslut = array();
+    foreach ($arr as $item) {
+        for ($i = 0; $i < count($reslut); $i++) {
+            if (!$f) {
+                if ($item[$key] > $reslut[$i][$key]) break;
+              } else {
+              if ($item[$key] < $reslut[$i][$key]) break;
+            }
+          }
+        Tools::insertArray($reslut, $i, $item);
       }
-      Tools::insertArray($reslut,$i,$item);
-    }
-    $arr=$reslut;
-  } 
+    $arr = $reslut;
+  }
   /**
    * 得到Array内指定的键，并生成相应的数组
    */
-  public static function GetValueByKey($arr,$key)
+  public static function GetValueByKey($arr, $key)
   {
-    try{
-      $reslut=array();
-      foreach($arr as $temp)
-      {
-        if(array_key_exists($key,$temp))
-          array_push($reslut,$temp[$key]);
-      }
+    try {
+      $reslut = array();
+      foreach ($arr as $temp) {
+          if (array_key_exists($key, $temp))
+            array_push($reslut, $temp[$key]);
+        }
       return $reslut;
-    }
-    catch(Exception $e){
+    } catch (Exception $e) {
       return [];
     }
   }
@@ -80,47 +73,53 @@ class  Tools
    * 向输入内指定位置插入元素
    * @param $pos 插入位置的下标，-2为插入count($arr)-1
    */
-  public static function insertArray(&$arr,$pos,$item)
+  public static function insertArray(&$arr, $pos, $item)
   {
-    $len=count($arr);
-    if($pos<0) $pos=$len+$pos+1;
-    $temp=$item;
-    for($i=$pos;$i<$len;$i++)
-    {
-      $t=$arr[$i];
-      $arr[$i]=$temp;
-      $temp=$t;
-    }
-    $arr[$i]=$temp;
+    $len = count($arr);
+    if ($pos < 0) $pos = $len + $pos + 1;
+    $temp = $item;
+    for ($i = $pos; $i < $len; $i++) {
+        $t = $arr[$i];
+        $arr[$i] = $temp;
+        $temp = $t;
+      }
+    $arr[$i] = $temp;
     return $arr;
   }
 
   /** 针对于Keyword操作合并相同的Key，并相加Weight */
   public static function mergeKeyWeight(&$arr)
   {
-    $reslut=array();
-    
-    foreach($arr as $item)
-    {
-      for($i=0;$i<count($reslut);$i++)
-      {
-        if($reslut[$i]["Id"]==$item["Id"]) break;
+    $reslut = array();
+
+    foreach ($arr as $item) {
+        for ($i = 0; $i < count($reslut); $i++) {
+            if ($reslut[$i]["Id"] == $item["Id"]) break;
+          }
+        if ($i == count($reslut))
+          Tools::insertArray($reslut, 0, $item);
+        else
+          $reslut[$i]["Weight"] = $reslut[$i]["Weight"] + $item["Weight"];
       }
-      if($i==count($reslut))
-        Tools::insertArray($reslut,0,$item);
-      else
-        $reslut[$i]["Weight"]= $reslut[$i]["Weight"]+$item["Weight"];
-    }
-    $arr=$reslut;
+    $arr = $reslut;
     return $reslut;
   }
-  
+
   /**懒加载计算数据查找条数范围 
    * @param pag 页数
    * @param num 每一页数量
   */
-  public static function getPageRange($pag,$num)
+  public static function getPageRange($pag, $num)
   {
-    return ($pag-1)*$num;
+    return ($pag - 1) * $num;
+  }
+  /**中文字符串转数组 */
+  public static function chToArray($str)
+  {
+    $length = mb_strlen($str, 'utf-8');
+    $array = [];
+    for ($i = 0; $i < $length; $i++)
+      $array[] = mb_substr($str, $i, 1, 'utf-8');
+    return $array;
   }
 }
